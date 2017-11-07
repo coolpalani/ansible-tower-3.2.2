1ac54d1e064cdabd2b7f5c828cf0ccdb3b570271
palaniecestar@gmail.com <palaniecestar@gmail.com>
2017-11-08 02:06:41 +0530

    first commit

---
+++ build.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker build -t ansible-tower:3.2.1 .
---
+++ start.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#get script path
+pushd `dirname $0` > /dev/null
+SCRIPTPATH=`pwd`
+popd > /dev/null
+ 	
+echo $SCRIPTPATH
+
+docker run -d \
+	-P \
+	--name tower \
+	-v "$SCRIPTPATH/license.txt":/etc/tower/license \
+	-v tower_certs:/certs \
+	-v tower_db:/var/lib/postgresql \
+	ansible-tower:3.2.1
+
+docker logs -f tower
